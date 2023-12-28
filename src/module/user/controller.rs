07625b5edf6016fe1fd954b9087f83369623cd01
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-28 19:02:11 +0100

    fixe double user creation

--- src/module/user/controller.rs
+++ src/module/user/controller.rs
@@ -1,35 +1,47 @@
-use crate::{module::user::data::user_types::User, utils::file::json::read_json};
+use crate::{
+    module::user::data::user_types::User,
+    module::user::service::get_user_with_username,
+    utils::file::json::{read_json, write_json},
+};
+
 use actix_web::{
     get, post,
     web::{self, Json},
     HttpResponse, Responder, Scope,
 };
-use std::fs;
 
-#[get("")]
-async fn get_all_user() -> impl Responder {
-    let user1: User = User::new("".to_string(), "".to_string());
-    HttpResponse::Ok().json(user1)
+#[get("/{username}")]
+async fn get_user(username: web::Path<String>) -> impl Responder {
+    match get_user_with_username(username.into_inner()) {
+        Ok(res) => match res {
+            Some(user) => return HttpResponse::Ok().body(serde_json::to_string(&user).unwrap()),
+            None => return HttpResponse::NotFound().body("No user found"),
+        },
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
 }
 
 #[post("")]
 async fn create_user(user: Json<User>) -> impl Responder {
-    let mut users: Vec<User> = read_json("users.json").expect("Unable to get users");
-    users.push(user.into_inner());
-
-    let users_string = match serde_json::to_string(&users) {
-        Ok(json) => json,
+    let mut users: Vec<User> = match read_json("users.json") {
+        Ok(users) => users,
         Err(_) => return HttpResponse::InternalServerError().finish(),
     };
 
-    match fs::write("users.json", &users_string.as_bytes()) {
+    let exist = users.iter().any(|f| f.username == user.username);
+
+    if exist {
+        return HttpResponse::InternalServerError().body("User already created");
+    }
+
+    users.push(user.into_inner());
+
+    match write_json("users.json", users) {
         Ok(_) => HttpResponse::Ok().body("user saved"),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+        Err(_) => return HttpResponse::InternalServerError().finish(),
     }
 }
 
 pub fn user_controller() -> Scope {
-    return web::scope("/users")
-        .service(get_all_user)
-        .service(create_user);
+    return web::scope("/users").service(get_user).service(create_user);
 }
