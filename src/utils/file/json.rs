db34f28d37755ec1803fc0ee64c6d28158865a48
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-28 19:03:43 +0100

    new function to read and write json

--- src/utils/file/json.rs
+++ src/utils/file/json.rs
@@ -1,9 +1,16 @@
 use std::{fs, io::Error};
 
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 
 pub fn read_json<T: DeserializeOwned>(path: &str) -> Result<T, Error> {
     let content = fs::read_to_string(path)?; // Utilise ? pour propager l'erreur
     let parsed = serde_json::from_str::<T>(&content)?; // Utilise ? pour propager l'erreur
     Ok(parsed)
+}
+pub fn write_json<T: Serialize>(path: &str, element: T) -> Result<&str, Error> {
+    let content_string = serde_json::to_string(&element)?;
+    match fs::write(path, content_string.as_bytes()) {
+        Ok(_) => Ok("Write ok"),
+        Err(e) => Err(e),
+    }
 }
