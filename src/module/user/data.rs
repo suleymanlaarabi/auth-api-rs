94b56f5b0ac60127facdfc8b8d26e1b4e77b7909
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-28 19:02:33 +0100

    remove unused constructor and add Clone derive

--- src/module/user/data.rs
+++ src/module/user/data.rs
@@ -1,14 +1,9 @@
 pub mod user_types {
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Clone)]
     pub struct User {
-        username: String,
-        password: String,
-    }
-    impl User {
-        pub fn new(username: String, password: String) -> User {
-            User { username, password }
-        }
+        pub username: String,
+        pub password: String,
     }
 }
