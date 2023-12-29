8fa2c1450f92c0f7e9e128d31f20fa586be6d941
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-29 17:25:53 +0100

    create new sqlite connection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use actix_web::{App, HttpServer};
 mod module;
 use module::user;
+use orm::{db::establish_connection, users::create_user};
+mod orm;
 mod utils;
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+pub mod schema;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let connection = &establish_connection();
+
     HttpServer::new(|| App::new().service(user::user_controller()))
         .bind(("127.0.0.1", 8080))?
         .run()
