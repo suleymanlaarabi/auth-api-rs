074552998df2fd134b54da9e774011ea93897413
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-12-29 17:27:03 +0100

    models dor users and posts table

---
+++ src/orm/models.rs
@@ -0,0 +1,14 @@
+use super::super::schema::users;
+#[derive(Queryable)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub password: &'a str,
+}
